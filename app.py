9f563fcd79c003af141f980110f18c6ff95db7d1
Suraj Kumar <surajhmss@gmail.com>
2022-01-23 22:33:04 +0530

    Hoga koi commit

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 
 from flask import Flask , render_template , request , send_file, url_for
 import pytube
-import logging
 import sys
 import os
+import glob
 from pytube import YouTube
 app = Flask(__name__)
 
+for name in glob.glob('Videos/*.mp4', recursive = True):
+    print(name)
+    os.remove(name)
 @app.route("/", methods=["POST","GET"])
 def homefnc():
+    
     if request.method == "POST":
-        try:
-            youtube_url = request.form["url1"]
-            path = 'E://'
-            local_download_path = YouTube(youtube_url).streams[1].download(path)
-            fname = local_download_path
-            return send_file(fname, as_attachment=True)
-            return render_template("index.html", cont="Downloading")
-        except:
-            logging.exception("Failed download")
-            return "Video download failed!"
+        youtube_url = request.form["url1"]
+        path = "Videos"
+        local_download_path = YouTube(youtube_url).streams[1].download(path)
+        fname = local_download_path
+        return send_file(fname, as_attachment=True)
     else:
         return render_template("index.html")
 
